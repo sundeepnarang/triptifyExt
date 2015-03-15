e92995adcbbc25f0e83f095698850ee0a86cedd3
Sundeep Narang <sundeepnrng@gmail.com>
2015-03-15 00:21:13 -0400

    server linked

--- background.js
+++ background.js
@@ -1,12 +1,41 @@
 //The code that is fired upon page load
 //to check your plugin js is working uncomment the next line.
+
 urlObject = window.location;
 if(urlObject.host=="www.expedia.com"){
     switch (urlObject.pathname){
         case "/Hotel-Search":
             //var hotelOptions=setHotelOption(urlObject.hash);
             $("body").livequery( ".hotelWrapper", function(elem){
-                $(elem).find("article").prepend("<div class='triptify-plus'>+</div>");
+                $(elem).find("article").prepend("<div class='triptify-plus')'>+</div>");
+                $(elem).find("article").find(".triptify-plus").click(function(){
+                    var hotelData= {}
+                    var hoteldiv = $(this).parent();
+                    hotelData.hotelId = hoteldiv.attr('data-hotelid');
+                    hotelData.link = hoteldiv.find('.target').attr('href');
+                    hotelData.picture = "http:" + hoteldiv.find('.target').find('.thumbnail').attr('data-image');
+                    hotelData.name = hoteldiv.find('.target').find('span.hotelName').text();
+                    hotelData.rating = hoteldiv.find('.target').find('li.starRating').find('.visuallyhidden').text();
+                    hotelData.price = hoteldiv.find('.target').find('.infoCol').find('.actualPrice').text();
+                    that = this;
+                    chrome.storage.sync.get('id',function(data){
+                        var form = {id:data.id,data:hotelData};
+                        var xmlhttp=new XMLHttpRequest();
+                        xmlhttp.onreadystatechange = function()
+                        {
+                            if (xmlhttp.readyState==4 && xmlhttp.status==200)
+                            {
+
+                                $(that).html("&#10004;");
+
+                            }
+                        };
+                        xmlhttp.open("POST","http://triptify.co:3000/save",true);
+                        xmlhttp.setRequestHeader('Content-type', 'application/json');
+                        form = JSON.stringify(form);
+                        xmlhttp.send(form);
+                    })
+                })
             } );
             break;
     }
---
+++ popup.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Sundeep on 3/14/2015.
+ */
+$(function(){
+    $( "form#triptify" ).submit(function( event ) {
+
+        event.preventDefault();
+        var form = $( this ).serialize();
+        var xmlhttp=new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function()
+        {
+            if (xmlhttp.readyState==4 && xmlhttp.status==200)
+            {
+
+                chrome.storage.sync.set({'id': xmlhttp.responseText},function(){
+                    $('#uid').append("<h1>Thanks!</h1>");
+                });
+
+            }
+        };
+        xmlhttp.open("POST","http://triptify.co:3000/extLogin",true);
+        xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+        xmlhttp.send(form);
+    });
+});
